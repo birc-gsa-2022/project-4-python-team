e634d9565143fbb58e5344808ce9340f0af91c0c
azerata <smedemarkhammer@gmail.com>
2022-11-14 13:24:23 +0100

    Added a few mississippi tests

--- src/test_example.py
+++ src/test_example.py
@@ -1,6 +1,22 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
+from fm import bwt, compress
+
 
 def test_1984():
     assert 2 + 2 == 4
+
+
+def test_correct_bwt_construction():
+    x = "mississippi$"
+    cor_l = "ipssm$pissi"
+
+    assert bwt(x) == cor_l
+
+
+def test_compression():
+    x = "mississippi$"
+    cor_com = "1i1p2s1m1$1p1i2s1i"
+
+    assert compress(x) == cor_com
