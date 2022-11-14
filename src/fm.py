cc0fedbfdc4464b890462402fc46bc092e232185
azerata <smedemarkhammer@gmail.com>
2022-11-14 13:24:10 +0100

    started bwt and preprocessing

--- src/fm.py
+++ src/fm.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import argparse
+from dataclasses import dataclass
 import sys
 
 
@@ -31,6 +33,46 @@ def main():
             argparser.print_help()
             sys.exit(1)
         print(f"Search {args.genome} for {args.reads}")
+
+
+@dataclass
+class rotating_string:
+    x: str
+
+    def __getitem__(self, i) -> str:
+        return self.x[(i) % len(self.x)]
+
+    def _len__(self) -> int:
+        return len(self.x)
+
+
+def sa_construct(x: str) -> list[int]:
+    ''' 
+    Simple SA construction algorithm 
+
+    If we get a more efficient version running, this will be updated, but right now we just need a working version.
+    '''
+    suf = [x[i:] for i, _ in enumerate(x)]
+    suf = sorted(suf)
+    return [len(x) - len(i) for i in suf]
+
+
+def bwt(x: str) -> str:
+    "returns the last column of a BWT from string x"
+    sa = sa_construct(x)
+    x = rotating_string(x)
+
+    l = [x[-1-i] for i in sa]
+    return ''.join(l)
+
+
+def compress(x: str) -> str:
+    ''' compresses string x using run length encoding '''
+    rle = []
+    run = 1
+    for i, c in enumerate(x):
+
+        if c == x[i+1]
 
 
 if __name__ == '__main__':
